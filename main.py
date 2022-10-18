4a9778c74003cd551b434177969de305f326b2e8
wjmint <jamie060529@gmail.com>
2022-10-18 21:37:39 +0900

    font added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Reference : https://github.com/yevini118/choco_sweeper/blob/main
+# Reference : https://github.com/yevini118/choco_sweeper/blob/main/choco_sweeper_v2.py
 
 import random, pygame, sys, time
 from pygame.locals import *
@@ -14,6 +14,19 @@ XMARGIN = CELLSIZE
 YMARGIN = CELLSIZE * 5
 
 # RGB
+BGCOLOR =(213, 192, 170)
+LINECOLOR = (179, 140, 101)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 176, 80)
+DARKGREEN = (56, 87, 35)
+BLUE = (0, 112, 192)
+DARKBLUE = (0, 32, 96)
+RED = (255, 0, 0)
+DARKRED = (192, 0, 0)
+TRANSRED = (255, 0, 0, 60)
+PINK = (227, 134, 134)
+YELLOW = (255, 230, 153)
 
 # Syntax
 mine = -1
@@ -24,5 +37,19 @@ opened = 1
 def main():
 	global DISPLAYSURF, GAMEDISPLAY, GAMEWINDOWWIDTH, GAMEWINDOWHEIGHT, CELLWORTH, MINECOUNT, BASICFONT, FACEIMG
 
+	pygame.init()
+
 	DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), RESIZABLE)
-	BASICFONT = pygame.font.Font('')
+	BASICFONT = pygame.font.Font('source/CascadiaCode.ttf', 18)
+	pygame.display.set_caption('Minesweeper')
+	MINEIMG = pygame.image.load('')
+
+	CELLWIDTH, CELLHEIGHT, MINECOUNT = showLevelScreen()
+	GAMEWINDOWWIDTH, GAMEWINDOWHEIGHT = CELLSIZE * CELLWIDTH + XMARGIN * 2, CELLSIZE * CELLHEIGHT + YMARGIN + CELLSIZE
+	GAMEDISPLAYSURF = pygame.display.set_mode((GAMEWINDOWWIDTH, GAMEWINDOWHEIGHT), RESIZABLE)
+
+	while True:
+		runGame()
+
+def runGame():
+	resetButton, boardSurf = drawBorder()
